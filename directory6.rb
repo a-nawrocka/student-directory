49a528ac168879ec43996f2575ada112c6434343
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-01-26 11:49:42 +0000

    Add step 6

--- directory6.rb
+++ directory6.rb
@@ -1,35 +1,36 @@
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort) and born in #{student[:birth]}" 
+    puts ("#{index +1}. #{student[:name]} (#{student[:cohort]} cohort) and born in #{student[:birth]}").center(50) 
   end
 end 
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(40)
 end
 
 # print_header
 # print(students)
 #print_footer(students)
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(40)
+  puts "To finish, just hit return twice".center(40)
   students = []
   name = gets.chomp
   # while the name is not empty, repeat this code
   until name.empty? do
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    puts "Where this person was born"
+    puts "Now we have #{students.count} students".center(30)
+    puts "Where this person was born".center(30)
     place_of_birth = gets.chomp
     students[-1][:birth] = place_of_birth
     name = gets.chomp
   end
+  
   students 
 end
 
